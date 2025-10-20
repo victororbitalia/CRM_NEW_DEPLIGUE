4288f15a04f9d8dfc514c2f05b686466e24912b0
victororbitalia <victor@orbitalia.com>
2025-10-20 19:56:02 +0200

    v2

--- DEMO PROYECTO DESPLEGADO/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Habilitar standalone para Docker (optimiza el tamaño de la imagen)
-  output: 'standalone',
-}
-
-module.exports = nextConfig
--- DEMO PROYECTO DESPLEGADO/prisma/migrations/20241201000000_add_preferred_location_to_reservations/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Reservation" ADD COLUMN "preferredLocation" TEXT;
